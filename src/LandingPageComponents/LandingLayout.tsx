f91bf404c7fd062079f7f537daea235797b37361
elisate <dushimiyimanaelisa@gmail.com>
2025-12-18 15:23:48 +0200

    fix some change

--- src/LandingPageComponents/LandingLayout.tsx
+++ src/LandingPageComponents/LandingLayout.tsx
@@ -1,12 +1,22 @@
-import  {Outlet} from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
+
 function LandingLayout() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
   return (
     <>
-    <Navbar />
-    <div className='px-4 sm:px-6 md:px-8 lg:px-12 xl:pl-[3rem] xl:pr-[3rem]'><Outlet/></div>
-    <Footer />
+      <Navbar />
+      <div className='px-4 sm:px-6 md:px-8 lg:px-12 xl:pl-[3rem] xl:pr-[3rem] scroll-smooth'>
+        <Outlet />
+      </div>
+      <Footer />
     </>
   )
 }
